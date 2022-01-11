c2ce2b8360cea867993c8d4c212a5f9bfc8b0853
empratyush <codelab@pratyush.dev>
2022-01-11 15:05:52 +0530

    added functionality to foreground service
    now when it started it shows download in progress stuff via push notification and stop it self once all task is finished

--- app/src/main/java/org/grapheneos/apps/client/service/KeepAppActive.kt
+++ app/src/main/java/org/grapheneos/apps/client/service/KeepAppActive.kt
@@ -1,11 +1,53 @@
 package org.grapheneos.apps.client.service
 
+import android.app.Notification
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationManagerCompat
+import androidx.lifecycle.Observer
+import org.grapheneos.apps.client.App
+import org.grapheneos.apps.client.R
 
 class KeepAppActive : Service() {
 
+    private lateinit var notificationMgr: NotificationManagerCompat
+    private val app: App by lazy { applicationContext as App }
+
+    private val observer = Observer<Boolean> { isDownloadRunning ->
+        if (isDownloadRunning) stopService()
+    }
+
+
+    private fun stopService() {
+        stopSelf()
+        notificationMgr.cancelAll()
+    }
+
     override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        notificationMgr = NotificationManagerCompat.from(this)
+        app.updateServiceStatus(true)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        app.isDownloadRunning().observeForever(observer)
+        val notification = Notification.Builder(this, App.BACKGROUND_SERVICE_CHANNEL)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("few packages is being downloaded")
+            .setOnlyAlertOnce(true)
+            .build()
+        notification.flags = Notification.FLAG_FOREGROUND_SERVICE
+        startForeground(1, notification)
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        app.isDownloadRunning().removeObserver(observer)
+        app.updateServiceStatus(false)
+    }
 
 }
