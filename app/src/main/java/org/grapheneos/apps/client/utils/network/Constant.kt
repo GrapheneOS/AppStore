4080e2163170b6a0abe5dfbe2e87a1fbe4471a57
empratyush <codelab@pratyush.dev>
2022-01-19 20:10:11 +0530

    Metadata verification helper added
    it offers downgrade protection, no internet handling, eTag caching optimization

---
+++ app/src/main/java/org/grapheneos/apps/client/utils/network/Constant.kt
@@ -0,0 +1,8 @@
+package org.grapheneos.apps.client.utils.network
+
+const val BASE_URL = "https://apps.grapheneos.org"
+const val PACKAGE_DIR_URL = "${BASE_URL}/packages/"
+
+const val PUBLIC_KEY = "RWQtZwEu1br1lMh911L3yPOs97cQb9LOks/ALBbqGl21ul695ocWR/ir"
+const val TIMESTAMP = 1640198430
+const val CURRENT_VERSION = 0
