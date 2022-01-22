a6d5fda1277c61291780aa6319e3f9ea96b2d483
empratyush <codelab@pratyush.dev>
2022-01-22 14:36:30 +0530

    (added, removed and updated) few (lib and build options)
    enabled view binding : https://developer.android.com/topic/libraries/view-binding
    added : (coroutines, dagger, activity-kts, fragment-kts )
    coroutines for easy background and network operation
    dagger for dependency injection
    activity-kts && fragment-kts is needed for kotlin kts fun
    dataBinding to replace findViewById with compile time safe options
    navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,8 @@ if (useKeystoreProperties) {
 plugins {
     id("com.android.application")
     kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -48,7 +50,9 @@ android {
             }
         }
     }
-
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility(JavaVersion.VERSION_11)
         targetCompatibility(JavaVersion.VERSION_11)
@@ -60,12 +64,37 @@ android {
 }
 
 dependencies {
+    /*Default android*/
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
-    testImplementation("junit:junit:4.+")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("com.google.android.material:material:1.6.0-alpha02")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.fragment:fragment-ktx:1.4.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.preference:preference:1.1.1")
+    implementation("androidx.preference:preference-ktx:1.1.1")
+
+    /*Signify java */
+    implementation("org.bouncycastle:bcpkix-jdk15to18:1.70")
+
+    /*Kotlin coroutines*/
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+
+    /*Dagger Hilt*/
+    implementation("com.google.dagger:hilt-android:2.40.2")
+    kapt("com.google.dagger:hilt-android-compiler:2.40.2")
+
+    /*Default test suite*/
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
+
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
     }
 }
 
