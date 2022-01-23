8417e814db7f2eb13238c9a9a19dfee8a358d2c0
Jorge Carrillo <jorge@jorgecarrillo.dev>
2022-01-22 16:51:41 -0800

    Fix spelling of confirmation

--- app/src/main/java/org/grapheneos/apps/client/item/SeamlessUpdateResponse.kt
+++ app/src/main/java/org/grapheneos/apps/client/item/SeamlessUpdateResponse.kt
@@ -3,6 +3,6 @@ package org.grapheneos.apps.client.item
 data class SeamlessUpdateResponse(
     val updatedSuccessfully : List<String> = emptyList(),
     val failedToUpdate : List<String> = emptyList(),
-    val requireConformation : List<String> = emptyList(),
+    val requireConfirmation : List<String> = emptyList(),
     val executedSuccessfully : Boolean = false
 )
