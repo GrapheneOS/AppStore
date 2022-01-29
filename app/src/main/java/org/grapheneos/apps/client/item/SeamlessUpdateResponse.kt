1bf22441e6f32e6826e2445f9aa90af0d4749426
June <june@eridan.me>
2022-01-29 06:15:09 +0000

    Refactor some code to the Official Kotlin codestyle
    Signed-off-by: June <june@eridan.me>

--- app/src/main/java/org/grapheneos/apps/client/item/SeamlessUpdateResponse.kt
+++ app/src/main/java/org/grapheneos/apps/client/item/SeamlessUpdateResponse.kt
@@ -1,8 +1,8 @@
 package org.grapheneos.apps.client.item
 
 data class SeamlessUpdateResponse(
-    val updatedSuccessfully : List<String> = emptyList(),
-    val failedToUpdate : List<String> = emptyList(),
-    val requireConfirmation : List<String> = emptyList(),
-    val executedSuccessfully : Boolean = false
+    val updatedSuccessfully: List<String> = emptyList(),
+    val failedToUpdate: List<String> = emptyList(),
+    val requireConfirmation: List<String> = emptyList(),
+    val executedSuccessfully: Boolean = false
 )
