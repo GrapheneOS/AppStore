aebaf305783785880aa8d7d5fbd632c7dac97996
empratyush <codelab@pratyush.dev>
2022-02-08 12:16:03 +0530

    added navigation safe agrs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.1.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.0-alpha01")
     }
 }
 
