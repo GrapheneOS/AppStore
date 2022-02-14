460f27e7694f1618bc7be235949f2d8ec3048ca3
empratyush <codelab@pratyush.dev>
2022-02-14 11:42:25 +0530

    added switch channel screen

---
+++ app/src/main/java/org/grapheneos/apps/client/ui/switchChannel/SwitchChannel.kt
@@ -0,0 +1,50 @@
+package org.grapheneos.apps.client.ui.switchChannel
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import org.grapheneos.apps.client.App
+
+class SwitchChannel : DialogFragment() {
+
+    private val args by lazy {
+        navArgs<SwitchChannelArgs>().value
+    }
+    private val app: App by lazy {
+        requireContext().applicationContext as App
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val channels = mutableListOf<String>()
+        var preSelectedIndex = 0
+        var selectedIndex = preSelectedIndex
+        for (i in 0 until args.channels.size) {
+            val name = args.channels[i]
+            channels.add(name)
+            if (args.selectedChannel == name) {
+                preSelectedIndex = i
+            }
+        }
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setTitle("Select release channel")
+            .setSingleChoiceItems(channels.toTypedArray(), preSelectedIndex) { _, index ->
+                selectedIndex = index
+            }
+            .setCancelable(true)
+            .setPositiveButton("ok") { _, _ ->
+                app.handleOnVariantChange(args.pkgName, channels[selectedIndex])
+                findNavController().popBackStack()
+            }
+            .setNegativeButton("cancel") { _, _ ->
+                findNavController().popBackStack()
+            }
+
+        return dialog.create()
+    }
+
+}
