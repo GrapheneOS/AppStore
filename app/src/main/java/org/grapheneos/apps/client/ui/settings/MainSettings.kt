a57b73f81d191f803f1f7c79129f4d22c7ddde96
empratyush <codelab@pratyush.dev>
2022-02-16 09:43:20 +0530

    debloat main screen and navigate to details screen for other options
    Co-authored-by: oumno <53978854+oumno@users.noreply.github.com>
    Co-authored-by: empratyush <codelab@pratyush.dev>

--- app/src/main/java/org/grapheneos/apps/client/ui/settings/MainSettings.kt
+++ app/src/main/java/org/grapheneos/apps/client/ui/settings/MainSettings.kt
@@ -1,11 +1,25 @@
 package org.grapheneos.apps.client.ui.settings
 
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.doOnPreDraw
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.transition.MaterialSharedAxis
 import org.grapheneos.apps.client.R
 import org.grapheneos.apps.client.utils.sharedPsfsMgr.JobPsfsMgr
 
 class MainSettings : PreferenceFragmentCompat() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        postponeEnterTransition()
+        view.doOnPreDraw {
+            startPostponedEnterTransition()
+        }
+        exitTransition = MaterialSharedAxis(MaterialSharedAxis.Y, true)
+        reenterTransition = MaterialSharedAxis(MaterialSharedAxis.Y, false)
+        enterTransition = MaterialSharedAxis(MaterialSharedAxis.Y, false)
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         preferenceManager.sharedPreferencesName = JobPsfsMgr.AUTO_UPDATE_PREFERENCE
