29c6451e680e452a16df58c667b4196646eb2a07
June <june@eridan.me>
2022-02-20 21:03:10 +0000

    Use stable channel of material
    Signed-off-by: June <june@eridan.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.6.0-alpha02")
+    implementation("com.google.android.material:material:1.5.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.activity:activity-ktx:1.4.0")
     implementation("androidx.fragment:fragment-ktx:1.4.1")
