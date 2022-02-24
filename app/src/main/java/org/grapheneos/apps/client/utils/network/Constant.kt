7cc9501c72e9ee3626e2b335602bebafceafd563
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2022-02-24 11:59:26 +0000

    Update timestamp as of apps.grapheneos.org

--- app/src/main/java/org/grapheneos/apps/client/utils/network/Constant.kt
+++ app/src/main/java/org/grapheneos/apps/client/utils/network/Constant.kt
@@ -4,5 +4,5 @@ const val BASE_URL = "https://apps.grapheneos.org"
 const val PACKAGE_DIR_URL = "${BASE_URL}/packages/"
 
 const val PUBLIC_KEY = "RWQtZwEu1br1lMh911L3yPOs97cQb9LOks/ALBbqGl21ul695ocWR/ir"
-const val TIMESTAMP = 1640198430
+const val TIMESTAMP = 1645702792
 const val CURRENT_VERSION = 0
