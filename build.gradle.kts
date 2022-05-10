4eb196dbcd5b9074635e7d5db46de51c054c04fd
Daniel Micay <danielmicay@gmail.com>
2022-05-10 12:45:42 -0400

    update dagger to 2.42

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.41")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.2")
     }
 }
