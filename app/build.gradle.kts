582222767aebdc3889cf101de22e47ac4f5598f2
Daniel Micay <danielmicay@gmail.com>
2022-08-04 19:07:12 -0400

    update Dagger to 2.43.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,8 +97,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
-    implementation("com.google.dagger:hilt-android:2.43.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.43.1")
+    implementation("com.google.dagger:hilt-android:2.43.2")
+    kapt("com.google.dagger:hilt-android-compiler:2.43.2")
 
     // force newer version of dependency of dependency
     val lifecycleVersion = "2.5.0"
