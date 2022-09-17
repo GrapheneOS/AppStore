GrapheneOS/Apps

Explicitly not export the appsChangedReceiver

app/src/main/java/org/grapheneos/apps/client/App.kt
@@ -62,6 +62,7 @@ import org.grapheneos.apps.client.utils.PackageManagerHelper.Companion.pmHelper
 import org.grapheneos.apps.client.utils.isInstallBlockedByAdmin
 import org.grapheneos.apps.client.utils.isUninstallBlockedByAdmin
 import org.grapheneos.apps.client.utils.getPackageInfoCompat
+import org.grapheneos.apps.client.utils.registerReceiverNonExportedCompat
 import org.grapheneos.apps.client.utils.network.ApkDownloadHelper
 import org.grapheneos.apps.client.utils.network.MetaDataHelper
 import org.grapheneos.apps.client.utils.sharedPsfsMgr.JobPsfsMgr
@@ -924,7 +925,7 @@ class App : Application() {
         appsChangesFilter.addAction(Intent.ACTION_PACKAGE_REMOVED)
         appsChangesFilter.addDataScheme("package")
 
-        registerReceiver(appsChangesReceiver, appsChangesFilter)
+        registerReceiverNonExportedCompat(appsChangesReceiver, appsChangesFilter)
 
         registerActivityLifecycleCallbacks(ActivityLifeCycleHelper { activity ->
             isActivityRunning = activity

app/src/main/java/org/grapheneos/apps/client/utils/ContextCompat.kt
@@ -1,0 +1,23 @@
+package org.grapheneos.apps.client.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.os.Build
+
+// Maybe temporary until androidX core library is updated to 1.9.0 stable
+fun Context.registerReceiverExportedCompat(br: BroadcastReceiver, filter: IntentFilter) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        this.registerReceiver(br, filter)
+    } else {
+        this.registerReceiver(br, filter, Context.RECEIVER_EXPORTED)
+    }
+}
+
+fun Context.registerReceiverNonExportedCompat(br: BroadcastReceiver, filter: IntentFilter) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        this.registerReceiver(br, filter)
+    } else {
+        this.registerReceiver(br, filter, Context.RECEIVER_NOT_EXPORTED)
+    }
+}

GrapheneOS/AppStore

app/src/main/java/org/grapheneos/apps/client/App.kt
@@ -62,6 +62,7 @@ import org.grapheneos.apps.client.utils.PackageManagerHelper.Companion.pmHelper
 import org.grapheneos.apps.client.utils.isInstallBlockedByAdmin
 import org.grapheneos.apps.client.utils.isUninstallBlockedByAdmin
 import org.grapheneos.apps.client.utils.getPackageInfoCompat
+import org.grapheneos.apps.client.utils.registerReceiverNotExportedCompat
 import org.grapheneos.apps.client.utils.network.ApkDownloadHelper
 import org.grapheneos.apps.client.utils.network.MetaDataHelper
 import org.grapheneos.apps.client.utils.sharedPsfsMgr.JobPsfsMgr
@@ -917,7 +918,7 @@ class App : Application() {
         appsChangesFilter.addAction(Intent.ACTION_PACKAGE_REMOVED)
         appsChangesFilter.addDataScheme("package")
 
-        registerReceiver(appsChangesReceiver, appsChangesFilter)
+        registerReceiverNotExportedCompat(appsChangesReceiver, appsChangesFilter)
 
         registerActivityLifecycleCallbacks(ActivityLifeCycleHelper { activity ->
             isActivityRunning = activity

app/src/main/java/org/grapheneos/apps/client/utils/ContextCompat.kt
@@ -1,0 +1,23 @@
+package org.grapheneos.apps.client.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.os.Build
+
+// Maybe temporary until androidX core library is updated to 1.9.0 stable
+fun Context.registerReceiverExportedCompat(br: BroadcastReceiver, filter: IntentFilter) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        this.registerReceiver(br, filter)
+    } else {
+        this.registerReceiver(br, filter, Context.RECEIVER_EXPORTED)
+    }
+}
+
+fun Context.registerReceiverNotExportedCompat(br: BroadcastReceiver, filter: IntentFilter) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        this.registerReceiver(br, filter)
+    } else {
+        this.registerReceiver(br, filter, Context.RECEIVER_NOT_EXPORTED)
+    }
+}