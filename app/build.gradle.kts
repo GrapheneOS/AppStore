GrapheneOS/AppStore

commit a3bdc7fe76e57003ee4bc4e13dc73cdc96186458

Bump bcpkix-jdk15to18 from 1.71 to 1.72
Bumps [bcpkix-jdk15to18](https://github.com/bcgit/bc-java) from 1.71 to 1.72.
- [Release notes](https://github.com/bcgit/bc-java/releases)
- [Changelog](https://github.com/bcgit/bc-java/blob/master/docs/releasenotes.html)
- [Commits](https://github.com/bcgit/bc-java/commits)

---
updated-dependencies:
- dependency-name: org.bouncycastle:bcpkix-jdk15to18
  dependency-type: direct:production
  update-type: version-update:semver-minor
...

Signed-off-by: dependabot[bot] <support@github.com>

app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
 
     implementation("com.google.android.material:material:1.6.1")
 
-    implementation("org.bouncycastle:bcpkix-jdk15to18:1.71")
+    implementation("org.bouncycastle:bcpkix-jdk15to18:1.72")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")

commit b841b5b804010d53877551e5f336b3b1a35411d1

Exclude extraneous bouncycastle .properties files
The update to bouncycastle 1.72 in a3bdc7f inadvertently added about 4
MiB of .properties files (as a result of bouncycastle's new Picnic and
SIKE implementations) to optimized release builds, increasing their size
by ~168%. Since we're not using bouncycastle for these algorithms,
exclude the extraneous files to reduce app size.

app/build.gradle.kts
@@ -77,6 +77,12 @@ android {
             }
         }
     }
+    packagingOptions {
+        resources.excludes.addAll(listOf(
+            "org/bouncycastle/pqc/**.properties",
+            "org/bouncycastle/x509/**.properties",
+        ))
+    }
 }
 
 dependencies {