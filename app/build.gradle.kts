c0b25e05afef861295903caf835287744e1aba7e
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2022-12-30 18:27:16 +0200

    update fragment-ktx to 1.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.5.4")
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation("androidx.preference:preference:1.2.0")
