e7f73d377d049498769b20107b020a5dc3bf81f1
Daniel Micay <danielmicay@gmail.com>
2023-02-08 14:44:57 -0500

    update AndroidX appcompat library to 1.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.6.1")
     implementation("androidx.fragment:fragment-ktx:1.5.5")
