c2a60d1cfb988ec90efea4a0cbd9e0a9496e810d
Daniel Micay <danielmicay@gmail.com>
2023-04-16 19:41:20 -0400

    update AndroidX Core-ktx to 1.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,7 @@ android {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.20")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.6.1")
