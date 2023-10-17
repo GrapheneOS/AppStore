425a530a8e4a96b4d93606e27606a1a87cdd34ee
Daniel Micay <daniel.micay@grapheneos.org>
2023-10-17 09:04:45 -0400

    update Glide to 4.16.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,7 +119,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
 
-    val glideVersion = "4.15.1"
+    val glideVersion = "4.16.0"
     implementation("com.github.bumptech.glide:glide:$glideVersion")
     ksp("com.github.bumptech.glide:ksp:$glideVersion")
 }
