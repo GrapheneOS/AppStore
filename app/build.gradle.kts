2cebe372f45b22299c7c484832a37084b68457e9
Daniel Micay <daniel.micay@grapheneos.org>
2023-11-15 13:15:45 -0500

    update Bouncy Castle to 1.77

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,7 +108,7 @@ dependencies {
 
     implementation("com.google.android.material:material:1.10.0")
 
-    implementation("org.bouncycastle:bcprov-jdk18on:1.76")
+    implementation("org.bouncycastle:bcprov-jdk18on:1.77")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
