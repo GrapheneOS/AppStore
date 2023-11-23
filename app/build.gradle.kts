e1554456e2d0194b44bc46152d3ca7d5c69c3401
Daniel Micay <daniel.micay@grapheneos.org>
2023-11-23 04:07:52 -0500

    update AndroidX Navigation KTX libraries to 2.7.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,8 +101,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.8.1")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
