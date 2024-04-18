2c5d2d3fd0fe709182bb780ffb348f3c318a9ce4
Daniel Micay <daniel.micay@grapheneos.org>
2024-04-18 13:12:19 -0400

    update AndroidX Activity KTX to 1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.activity:activity-ktx:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
