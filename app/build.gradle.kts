9a09faf1b1dc154cfbbcfa3045a676ec7c43a0d4
Daniel Micay <daniel.micay@grapheneos.org>
2024-04-26 15:43:54 -0400

    increment version to 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
         applicationId = "app.grapheneos.apps"
         minSdk = 31
         targetSdk = 34
-        versionCode = 22
+        versionCode = 23
         versionName = versionCode.toString()
         resourceConfigurations.add("en")
 
