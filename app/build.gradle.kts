9302916d798d1cc2daa2a865bd08dfa6f8636a05
Daniel Micay <daniel.micay@grapheneos.org>
2024-05-02 15:34:56 -0400

    update Material Components library to 1.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,7 +97,7 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("org.bouncycastle:bcprov-jdk18on:1.78.1")
 
