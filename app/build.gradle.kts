bbfa1f1a621bf4967004702d2152964caeaa809e
Daniel Micay <daniel.micay@grapheneos.org>
2024-05-29 20:59:19 -0400

    update AndroidX Lifecycle libraries to 2.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,7 +105,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     // force newer version of dependency of dependency
-    val lifecycleVersion = "2.7.0"
+    val lifecycleVersion = "2.8.1"
     implementation("androidx.lifecycle:lifecycle-viewmodel:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
 
