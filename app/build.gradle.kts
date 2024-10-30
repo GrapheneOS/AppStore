6edaa9c78a7de207719cf224021635b96bb46171
Daniel Micay <daniel.micay@grapheneos.org>
2024-10-30 17:34:16 -0400

    update AndroidX Core KTX to 1.15.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.9.2")
