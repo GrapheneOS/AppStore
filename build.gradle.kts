357a5bc576951426e6863eb6d14697fa24c0b7ea
Daniel Micay <daniel.micay@grapheneos.org>
2025-03-03 12:54:11 -0500

    update AndroidX Navigation libraries/plugin to 2.8.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.8.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("androidx.navigation.safeargs") version "2.8.7" apply false
+    id("androidx.navigation.safeargs") version "2.8.8" apply false
     id("com.google.devtools.ksp") version "2.1.10-1.0.30" apply false
 }
 
