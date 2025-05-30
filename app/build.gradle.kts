3e7f3e2d09e75e3859e6f6a3630a651f8bd79832
Daniel Micay <daniel.micay@grapheneos.org>
2025-05-30 19:23:50 -0400

    update AndroidX Navigation libraries/plugin to 2.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,8 +97,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.fragment:fragment-ktx:1.8.7")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.8")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.8")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false
-    id("androidx.navigation.safeargs") version "2.8.8" apply false
+    id("androidx.navigation.safeargs") version "2.9.0" apply false
     id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
 }
 
