0a08f61a621e473d7f620d9a9244a0ef2a8ef0c5
Daniel Micay <daniel.micay@grapheneos.org>
2025-05-30 19:16:56 -0400

    update AndroidX Core KTX to 1.16.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.activity:activity-ktx:1.10.1")
