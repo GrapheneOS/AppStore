09a8d3e0f76b25b184e2ae41c47d68c4844b9b14
Daniel Micay <daniel.micay@grapheneos.org>
2025-06-08 22:44:11 -0400

    update AndroidX Appcompat to 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,7 +93,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.fragment:fragment-ktx:1.8.7")
