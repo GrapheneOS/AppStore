229bd7c6b7515ae50606cdadc2f7bf3d56bb0d6c
Daniel Micay <daniel.micay@grapheneos.org>
2025-11-08 18:45:30 -0500

    update Kotlin to 2.2.21 and KSP to 2.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
     id("androidx.navigation.safeargs") version "2.9.3" apply false
-    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
 }
 
 allprojects {
