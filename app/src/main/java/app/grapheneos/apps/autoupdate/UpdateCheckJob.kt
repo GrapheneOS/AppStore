f6e511e51f4ef9b88a07eebebd5f54a4dc47c1d5
Axel Karlsson <git@axelkarlsson.se>
2026-04-15 14:50:11 +0200

    add missing imports

--- app/src/main/java/app/grapheneos/apps/autoupdate/UpdateCheckJob.kt
+++ app/src/main/java/app/grapheneos/apps/autoupdate/UpdateCheckJob.kt
@@ -1,8 +1,10 @@
 package app.grapheneos.apps.autoupdate
 
 import android.app.Notification
+import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
 import android.text.format.Formatter
 import android.util.Log
 import androidx.navigation.NavDeepLinkBuilder
