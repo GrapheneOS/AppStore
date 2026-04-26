13098da264867db507c9bd897ddb234491facd19
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-04-26 15:17:21 +0000

    remove unused getPendingUniqueActivityIntent() function

--- app/src/main/java/app/grapheneos/apps/util/IntentUtils.kt
+++ app/src/main/java/app/grapheneos/apps/util/IntentUtils.kt
@@ -1,12 +1,10 @@
 package app.grapheneos.apps.util
 
-import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Build
 import android.os.Parcelable
 import app.grapheneos.apps.core.appContext
-import java.util.UUID
 
 inline fun <reified T : Parcelable> getParcelableExtra(intent: Intent, name: String): T? {
     return if (Build.VERSION.SDK_INT >= 34) {
@@ -29,8 +27,3 @@ inline fun <reified T : Parcelable> getParcelableArrayListExtra(intent: Intent, 
 inline fun <reified T> intent() = Intent(appContext, T::class.java)
 
 inline fun <reified T> componentName() = ComponentName(appContext, T::class.java)
-
-fun getPendingUniqueActivityIntent(intent: Intent, flags: Int): PendingIntent {
-    intent.identifier = UUID.randomUUID().toString()
-    return PendingIntent.getActivity(appContext, 0, intent, flags)
-}
